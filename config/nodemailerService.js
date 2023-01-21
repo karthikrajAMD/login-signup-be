b733fc168585f7a3b83f5301a260e240080b67ce
Karthik Raja <karthikrajaamd@gmail.com>
2023-01-21 12:57:57 +0530

    msg

--- config/nodemailerService.js
+++ config/nodemailerService.js
@@ -21,17 +21,10 @@ const nodemailerService = async (toAddress, myLink) => {
     let info = await transporter.sendMail({
       from: "karthikraja.a.ece@gmail.com", // sender address
       to: `${toAddress}`, // list of receivers
-      subject: "Hello ✔", // Subject line
-      text: "Hello world?", // plain text body
+      subject: "Your OTP to Reset", // Subject line
+      text: "User Password Reset ", // plain text body
       html: `<b>${myLink}</b>`, // html body
     });
-
-    console.log("Message sent: %s", info.messageId);
-    // Message sent: <b658f8ca-6296-ccf4-8306-87d57a0b4321@example.com>
-
-    // Preview only available when sending through an Ethereal account
-    console.log("Preview URL: %s", nodemailer.getTestMessageUrl(info));
-    // Preview URL: https://ethereal.email/message/WaQKMgKddxQDoou...
   } catch (error) {
     console.error("error da", error);
   }
